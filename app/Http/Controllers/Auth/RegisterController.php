3354f43062f6065c7c53076f2c5345b0e90d0dcf
l4w <valdezlawrenze@gmail.com>
2018-07-26 00:45:35 +0800

    Controller fixes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/user/dashboard';
 
     /**
      * Create a new controller instance.
