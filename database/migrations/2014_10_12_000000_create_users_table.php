ca77ea057e9a4705a41e54b3bcf3e0a8a72ef787
L4w <valdezlawrenze@gmail.com>
2018-07-25 23:18:35 +0800

    Initial commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname', 20);
+            $table->string('lastname', 20);
+            $table->string('username', 15)->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('gender', 5);
+            $table->integer('role_id')->default(1);
+            $table->softDeletes();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
